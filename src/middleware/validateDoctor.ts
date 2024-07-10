7185070b481fc8cf206f29878602920e9faece40
Sebastian Jimenez <jimenez1121monory@gmail.com>
2024-07-10 11:35:29 -0500

    fix: corregir errores en validaciones de doctor

--- src/middleware/validateDoctor.ts
+++ src/middleware/validateDoctor.ts
@@ -1,61 +1,62 @@
 import { body } from 'express-validator';
 import { dominiosPermitidos } from '../constants';
 
-const doctorValidator = ()=>{ 
-    return[
-    body('tarjetaProf')
-    .notEmpty()
-    .withMessage('La tarjeta profesional es requerida'),
+const doctorValidator = () => { 
+    return [
+        body('tarjetaProf')
+            .notEmpty()
+            .withMessage('La tarjeta profesional es requerida'),
 
-    body('documento')
-    .notEmpty()
-    .withMessage('El documento es requerido'),
+        body('documento')
+            .notEmpty()
+            .withMessage('El documento es requerido'),
 
-    body('nombre')
-    .notEmpty()
-    .withMessage('El nombre es requerido'),
+        body('nombre')
+            .notEmpty()
+            .withMessage('El nombre es requerido'),
 
-    body('apellido')
-    .notEmpty()
-    .withMessage('El apellido es requerido'),
+        body('apellido')
+            .notEmpty()
+            .withMessage('El apellido es requerido'),
 
-    body('rol')
-    .notEmpty()
-    .isIn(['doctor'])
-    .withMessage('El rol es requerido'),
+        body('rol')
+            .notEmpty()
+            .isIn(['medico'])
+            .withMessage('El rol es requerido y debe ser "doctor"'),
 
-    body('email')
-        .isEmail()
-        .withMessage('Debe ser un correo electrónico válido')
-        .custom(value => {
-            const dominio = value.split('@')[1];
-            if (!dominiosPermitidos.includes(dominio)) {
-                throw new Error('Dominio de correo no permitido');
-            }
-            return true;
-        }),
+        body('email')
+            .isEmail()
+            .withMessage('Debe ser un correo electrónico válido')
+            .custom(value => {
+                const dominio = value.split('@')[1];
+                if (!dominiosPermitidos.includes(dominio)) {
+                    throw new Error('Dominio de correo no permitido');
+                }
+                return true;
+            }),
 
+        /* body('foto').optional()
+            .isURL()
+            .withMessage('La foto debe ser una URL válida'), */
 
-    /* body('foto').optional()
-    .isURL()
-    .withMessage('La foto debe ser una URL válida'), */
+        body('password')
+            .isLength({ min: 8 })
+            .withMessage('La contraseña debe tener al menos 8 caracteres')
+            .matches(/\d/)
+            .withMessage('La contraseña debe contener al menos un número')
+            .matches(/[a-z]/)
+            .withMessage('La contraseña debe contener al menos una letra minúscula')
+            .matches(/[A-Z]/)
+            .withMessage('La contraseña debe contener al menos una letra mayúscula')
+            .matches(/[!@#$%^&*(),.?":{}|<>]/)
+            .withMessage('La contraseña debe contener al menos un carácter especial'),
 
-    body('password')
-        .isLength({ min: 8 })
-        .withMessage('La contraseña debe tener al menos 8 caracteres')
-        .matches(/\d/)
-        .withMessage('La contraseña debe contener al menos un número')
-        .matches(/[a-z]/)
-        .withMessage('La contraseña debe contener al menos una letra minúscula')
-        .matches(/[A-Z]/)
-        .withMessage('La contraseña debe contener al menos una letra mayúscula')
-        .matches(/[!@#$%^&*(),.?":{}|<>]/)
-        .withMessage('La contraseña debe contener al menos un carácter especial'),
-
-    body('codigoEspc')
-    .notEmpty()
-    .isString()
-    .withMessage('El nombre debe ser una cadena de caracteres'),
-]};
+        body('codigoEspc')
+            .notEmpty()
+            .withMessage('El código de especialidad es requerido')
+            .isString()
+            .withMessage('El código de especialidad debe ser una cadena de caracteres'),
+    ];
+};
 
 export default doctorValidator;
